409177e16b55bc279c0f4258ddff6b930c673a4b
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-10-12 07:59:42 +0100

    added an overlay

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
 $("#icon-container").click(function (e) { 
    $(".navlist").toggle("");
    $("#iconMenu").toggleClass("mobile-menu-open .mobile-menu-close");
-  
+   $(".overlay").toggle();
+   
+});
+
+$("#feature").click(function (e) { 
+   e.preventDefault();
+   $(".inner-list-content").toggle("");
+   
+});
+$("#history").click(function (e) { 
+   e.preventDefault();
+   $(".inner-list-content-history").toggle("");
+   
 });
