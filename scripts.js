884d084d4e5b234adf33d9253475fd9e522bef6d
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-10-16 19:20:02 +0100

    completed

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $("#icon-container").click(function (e) { 
    $(".navlist").toggle("");
-   $("#iconMenu").toggleClass("mobile-menu-open .mobile-menu-close");
+   $("#iconMenu").toggleClass("mobile-menu-open mobile-menu-close");
    $(".overlay").toggle();
    
 });
@@ -8,10 +8,12 @@ $("#icon-container").click(function (e) {
 $("#feature").click(function (e) { 
    e.preventDefault();
    $(".inner-list-content").toggle("");
+   $("#cadet").toggleClass("icon-arrow-down icon-arrow-up");
    
 });
 $("#history").click(function (e) { 
    e.preventDefault();
    $(".inner-list-content-history").toggle("");
+   $("#company-cadet").toggleClass("icon-arrow-down icon-arrow-up");
    
 });
